9fc88c060c441dc93cfc4bd418c08042afe95169
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-07 17:40:40 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,6 @@
 class Author
   attr_accessor :name, :genre, :posts
+  @@post_count = 0
 
   def initialize(name)
     @name = name
@@ -11,7 +12,17 @@ class Author
   end
 
   def add_post(post)
-    self.posts(post)
+    self.posts = post
     post.author = self
+    @@post_count += 1
+  end
+
+  def add_post_by_title(post)
+    p = Post.new(post)
+    add_post(post)
+  end
+
+  def self.post_count
+    @@post_count
   end
 end
