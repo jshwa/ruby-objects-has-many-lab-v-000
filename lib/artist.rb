91f4dd9213ba8f3782da800636095cab4f7b2444
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-07 17:08:34 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,6 @@
 class Artist
   attr_accessor :name
+  @@song_count
 
   def initialize(name)
     @name = name
@@ -9,10 +10,15 @@ class Artist
   def add_song(song)
     @song << song
     song.artist = self
+    @@song_count += 1
   end
 
   def add_song_by_name(song)
     s = Song.new(song)
     add_song(s)
+  end
+
+  def self.song_count
+    @@song_count
   end
 end
