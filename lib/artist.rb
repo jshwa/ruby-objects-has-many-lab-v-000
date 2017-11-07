06d3287bc46a432d78cb358bddcee5068191fb12
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-07 17:02:38 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,18 @@
-class Post
-  attr_accessor :title, :author
+class Artist
+  attr_accessor :name
 
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @song << song
+    song.artist = self
+  end
+
+  def add_song_by_name(song)
+    s = Song.new(song)
+    add_song(s)
+  end
 end
